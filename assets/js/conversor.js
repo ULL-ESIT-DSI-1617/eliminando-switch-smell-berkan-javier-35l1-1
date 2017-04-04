797844f24e3d300aac8ac8762dbecfe5d5ef4f2a
berkanrhdz <alu0100815733@ull.edu.es>
2017-04-05 00:02:01 +0100

    Archivos css,js,html reestructurados

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,128 +1,128 @@
 "use strict";
 
-let Measurement = (function(){
+let Measurement = (function() {
 
     let _value = Symbol();
     let _magnitude = Symbol();
 
-    class Measurement{
-        constructor(value, magnitude){
+    class Measurement {
+        constructor(value, magnitude) {
             this[_value] = value;
             this[_magnitude] = magnitude;
         }
-        get value(){
+        get value() {
             return this[_value];
         }
-        get magnitude(){
+        get magnitude() {
             return this[_magnitude];
         }
     }
     return Measurement;
 }());
 
-class Temperature extends Measurement{
-    constructor(value, magnitude){
+class Temperature extends Measurement {
+    constructor(value, magnitude) {
         super(value, magnitude);
     }
 }
 
-class Celcius extends Temperature{
-    constructor(value, magnitude){
+class Celcius extends Temperature {
+    constructor(value, magnitude) {
         super(value, magnitude);
     }
 
-    toKelvin(){
-        return (this.value + 273.15).toFixed(2)+" Kelvin";
+    toKelvin() {
+        return (this.value + 273.15).toFixed(2) + " Kelvin";
     }
 
-    toFarenheit(){
-        return ((this.value * 9/5)+32).toFixed(2)+" Farenheit";
+    toFarenheit() {
+        return ((this.value * 9 / 5) + 32).toFixed(2) + " Farenheit";
     }
 }
 
-class Farenheit extends Temperature{
-    constructor(value, magnitude){
+class Farenheit extends Temperature {
+    constructor(value, magnitude) {
         super(value, magnitude);
     }
 
-    toKelvin(){
-        return ((this.value + 459.67) * 5/9).toFixed(2)+" Kelvin";
+    toKelvin() {
+        return ((this.value + 459.67) * 5 / 9).toFixed(2) + " Kelvin";
     }
 
-    toCelsius(){
-        return ((this.value - 32) / (9/5)).toFixed(2)+" Celcius";
+    toCelsius() {
+        return ((this.value - 32) / (9 / 5)).toFixed(2) + " Celcius";
     }
 }
 
-class Kelvin extends Temperature{
-    constructor(value, magnitude){
+class Kelvin extends Temperature {
+    constructor(value, magnitude) {
         super(value, magnitude);
     }
 
-    toCelsius(){
-        return (this.value - 273.15).toFixed(2)+" Celcius";
+    toCelsius() {
+        return (this.value - 273.15).toFixed(2) + " Celcius";
     }
 
-    toFarenheit(){
-        return ((this.value * 9/5)-459.67).toFixed(2)+" Farenheit";
+    toFarenheit() {
+        return ((this.value * 9 / 5) - 459.67).toFixed(2) + " Farenheit";
     }
 }
 
-function converter(){
+function converter() {
     var temp = original.value;
     var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcCkK])\s*(to)?\s*([fFcCkK])\s*$/;
 
     var m = temp.match(regexp);
 
-    if(m){
+    if (m) {
         var num = m[1];
         var fromType = m[2];
         var toType = m[4];
         num = parseFloat(num);
         if (fromType == 'c' || fromType == 'C') {
             var temp = new Celcius(num, fromType);
-            switch(toType){
+            switch (toType) {
                 case "k":
                 case "K":
                     converted.innerHTML = temp.toKelvin();
-                break;
+                    break;
                 case "f":
                 case "F":
                     converted.innerHTML = temp.toFarenheit();
-                break;
-                default: break;
+                    break;
+                default:
+                    break;
             }
-        }
-        else if (fromType == 'f' || fromType == 'F') {
+        } else if (fromType == 'f' || fromType == 'F') {
             var temp = new Farenheit(num, fromType);
-            switch(toType){
+            switch (toType) {
                 case "k":
                 case "K":
                     converted.innerHTML = temp.toKelvin();
-                break;
+                    break;
                 case "c":
                 case "C":
                     converted.innerHTML = temp.toCelsius();
-                break;
-                default: break;
+                    break;
+                default:
+                    break;
             }
-        }
-        else{
+        } else {
             var temp = new Kelvin(num, fromType);
-            switch(toType){
+            switch (toType) {
                 case "f":
                 case "F":
                     converted.innerHTML = temp.toFarenheit();
-                break;
+                    break;
                 case "c":
                 case "C":
                     converted.innerHTML = temp.toCelsius();
-                break;
-                default: break;
+                    break;
+                default:
+                    break;
             }
         }
-    }
-    else{
+    } else {
         converted.innerHTML = "ERROR! Try something like '-4.2C to f' or '-4.2C f' instead";
     }
 }
