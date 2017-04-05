aa440e307fb1f1602f976ee6cad9e74a88c4c42d
berkanrhdz <alu0100815733@ull.edu.es>
2017-04-05 11:03:48 +0100

    Añade FicherosJS (measurement.js temperature.js)

--- assets/js/measurement.js
+++ assets/js/measurement.js
@@ -1,5 +1,3 @@
-"use strict";
-
 let Measurement = (function() {
 
     let _value = Symbol();
@@ -19,54 +17,6 @@ let Measurement = (function() {
     }
     return Measurement;
 }());
-
-class Temperature extends Measurement {
-    constructor(value, magnitude) {
-        super(value, magnitude);
-    }
-}
-
-class Celcius extends Temperature {
-    constructor(value, magnitude) {
-        super(value, magnitude);
-    }
-
-    toKelvin() {
-        return (this.value + 273.15).toFixed(2) + " Kelvin";
-    }
-
-    toFarenheit() {
-        return ((this.value * 9 / 5) + 32).toFixed(2) + " Farenheit";
-    }
-}
-
-class Farenheit extends Temperature {
-    constructor(value, magnitude) {
-        super(value, magnitude);
-    }
-
-    toKelvin() {
-        return ((this.value + 459.67) * 5 / 9).toFixed(2) + " Kelvin";
-    }
-
-    toCelsius() {
-        return ((this.value - 32) / (9 / 5)).toFixed(2) + " Celcius";
-    }
-}
-
-class Kelvin extends Temperature {
-    constructor(value, magnitude) {
-        super(value, magnitude);
-    }
-
-    toCelsius() {
-        return (this.value - 273.15).toFixed(2) + " Celcius";
-    }
-
-    toFarenheit() {
-        return ((this.value * 9 / 5) - 459.67).toFixed(2) + " Farenheit";
-    }
-}
 
 function converter(original_measure) {
     var temp = original_measure;
