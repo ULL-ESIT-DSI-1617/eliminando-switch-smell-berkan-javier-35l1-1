bb697959c7fc00ab8f10f832855f613fcc445c5d
Javier <alu0100819441@ull.edu.es>
2017-04-16 17:22:13 +0100

    Crea clase Farenheit
    Se ha incluido:
    
    - Constructor.
    - Funcion para convertir a Celsius.
    - Funcion para convertir a Farenheit.
    - Funcion para comprobar la expresion regular.

--- assets/js/temperature.js
+++ assets/js/temperature.js
@@ -1,10 +1,10 @@
 (function(exports) {
     "use strict";
-
+    // Clase Temperature
     function Temperature(valor, tipo) {
         Medida.call(this, valor, tipo);
     }
-
+    // Clase Celsius
     function Celsius(valor) {
         Temperatura.call(this, valor, "c");
     }
@@ -23,9 +23,32 @@
         toCelsius: function(value) {
             return this.valor;
         }
+    
+    };
+
+    // Clase Farenheit
+    function Farenheit(valor){
+        Temperature.call(this, valor, "f");
+    }
+
+    Farenheit.prototype = {
+        name: "Farenheit",
+
+        check: function(tipo){
+            return tipo.match(/(f(?:a(?:h(?:r(?:e(?:n(?:h(?:e(?:i(?:t))))))))))/g);
+        },
+
+        toCelsius: function(value) {
+            return ((this.valor - 32)*5/9);
+        },
+
+        toFahrenheit: function(value) {
+            return this.valor;
+        }
     };
 
     exports.Temperatura = Temperature;
     exports.Celsius = Celsius;
+    exports.Farenheit = Farenheit;
 
-})(this)
+})(this);
